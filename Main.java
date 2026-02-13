bcb41cd7a8914f989f6bfff49384ff56753a6734
Jessica2784 <jv68663@gmail.com>
2026-02-12 19:27:28 -0600

    Subiendo practica de principios

---
+++ Main.java
@@ -0,0 +1,48 @@
+
+
+/**
+ * Class Main
+ */
+public class Main {
+
+  //
+  // Fields
+  //
+
+  
+  //
+  // Constructors
+  //
+  public Main () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  //
+  // Other methods
+  //
+
+  /**
+   * @param        args
+   */
+  public static void main(String [] args)
+  {
+
+	Persona persona = new Persona();
+	persona.saludar();
+	system.out,println(persona.getEdad());
+
+
+} 
+
+
+
+
+
+}

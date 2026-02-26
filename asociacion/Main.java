61e25152c74b20d3cb2eb2ac12b59c375c6627b1
Jessica2784 <jv68663@gmail.com>
2026-02-26 08:25:43 -0600

    Resuelto

---
+++ asociacion/Main.java
@@ -0,0 +1,36 @@
+
+
+/**
+ * Interface Main
+ */
+public class Main {
+	
+  // Fields
+  //
+
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  //
+  // Other methods
+  //
+
+  /**
+   * @param        args
+   */
+  public static void main(String args []){
+
+	Auto auto1 = new Auto("Toyota");
+        Persona p1 = new Persona("Jessica", auto1);
+        p1.mostrarAuto();
+
+
+}
+}
